5843feeb0614c488ec6323a7ae3d4617e42c5bf7
Anthony Achonye <tonexify@gmail.com>
2017-09-20 00:40:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,30 @@
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns true or false
+}
+
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     i += 1
     if (i === 1) {
       array.push(`I am 1 strange loop.`)
     } else {
-      array.push(`I am ${i} strange loop.`)
+      array.push(`I am ${i} strange loops.`)
     }
   }
+  return array
+}
+
+function whileLoop(n) {
+  while (n >= 0) {
+    console.log(n)
+  }
+  return 'done'
+}
+
+function doWhile(array) {
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue())
+
+  return array
 }
